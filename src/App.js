0689a029da4abc74de70770214a4a5ae5f61c4a1
thsurgeonofdeath <salahek25@gmail.com>
2023-02-17 15:05:20 +0100

    centered div

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ export default function App() {
     ))
     
     return (
-        <main className="">
+        <main>
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. 
